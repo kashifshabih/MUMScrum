5fb92d258fc7f09d10c3b2a655860726fda492ae
imrankhaan <imr_khan@outlook.com>
2015-05-12 00:18:45 -0400

    Master Layout Desing and Cutomize Login initial comming

---
+++ edu.mum.mumscrum/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace edu.mum.mumscrum.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] Sha1Password { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+
+      
+    }
+}
