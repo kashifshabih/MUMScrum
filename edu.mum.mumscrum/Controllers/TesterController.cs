06d817235d3e57fbfbdc0b4c6946f9993b159d08
kashifshabih <kashif.shabih@hotmail.com>
2015-05-18 15:50:44 -0500

    Tester Estimates Effort

---
+++ edu.mum.mumscrum/Controllers/TesterController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using edu.mum.mumscrum.DAL;
+using edu.mum.mumscrum.Models;
+
+
+namespace edu.mum.mumscrum.Controllers
+{
+    public class TesterController : Controller
+    {
+        private MUMScrumContext db = new MUMScrumContext();
+
+        // GET: Tester
+        public ActionResult Index()
+        {
+            string loggedInUserName = User.Identity.GetUserName();
+
+            var userStories = db.UserStories.Include(u => u.ProductBacklog).Include(u => u.ReleaseBacklog).Include(u => u.Sprint)
+                                .Where(u => u.SprintID != null && u.Tester.UserName == loggedInUserName);
+            return View(userStories.ToList());
+        }
+
+        public ActionResult EstimateEffort(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UserStory userStory = db.UserStories.Find(id);
+
+            if (userStory == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(userStory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EstimateEffort([Bind(Include = "ID,Name,Description,CreatedBy,CreatedDate,StartDate,ExpectedEndDate,ActualEndDate,DevelopmentStatus,TestStatus,SprintID,ReleaseBacklogID,ProductBacklogID,DeveloperID,TesterID,DeveloperEstimateInHours,TesterEstimateInHours,DeveloperHoursCompleted,TesterHoursCompleted")] UserStory userStory)
+        {
+            if (ModelState.IsValid)
+            {
+                if (userStory.TesterEstimateInHours == userStory.TesterHoursCompleted)
+                {
+                    userStory.TestStatus = USTestStatus.Completed;
+                }
+
+                db.Entry(userStory).State = EntityState.Modified;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userStory);
+        }
+    }
+}
